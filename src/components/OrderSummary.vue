26b99ed38e85e495e86294cc172d50cde82c18c4
Alex Chen <elkhouri1@gmail.com>
2026-05-04 20:37:47 +0800

    implement order summary

--- src/components/OrderSummary.vue
+++ src/components/OrderSummary.vue
@@ -1,8 +1,68 @@
 <script setup>
+import { computed } from 'vue';
+import { useAttendee } from '../composables/attendee';
+import { useAddons } from '../composables/addons';
+
+const { ticketType } = useAttendee()
+const { selectedAddons } = useAddons();
+
+const isVIP = computed(() => ticketType.value && ticketType.value.id === 'vip');
+
+const workshopDiscount = computed(() => {
+  if (!isVIP.value) return 0
+
+  return Object.values(selectedAddons.value).reduce((discount, addon) => {
+    if (addon.category === 'workshop') {
+      return discount + addon.price * 0.1;
+    }
+    return discount;
+  }, 0);
+});
+
+const totalPrice = computed(() => {
+  if (!ticketType.value) return 0;
+
+  const addonsTotal = Object.values(selectedAddons.value).reduce((total, addon) => {
+    if (addon.quantity) {
+      return total + addon.price * addon.quantity;
+    } else {
+      return total + addon.price;
+    }
+  }, 0);
+  const discount = workshopDiscount.value;
+  const total = ticketType.value.price + addonsTotal - discount;
+
+  return total;
+});
+
+const usdFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
 </script>
 
 <template>
   <div class="p-6 rounded-md border border-solid border-neutral-muted bg-surface-l1">
-    <div class="text-subtitle1">Order Summary</div>
+    <div class="mb-4 text-subtitle1">Order Summary</div>
+    <div class="mb-4 pb-4 space-y-4 border-0 border-b border-solid border-neutral-muted">
+      <div v-if="ticketType" class="flex justify-between">
+        <div class="text-sm text-neutral-muted">{{ ticketType.name }} Ticket</div>
+        <div class="text-sm text-neutral-muted">{{ usdFormatter.format(ticketType.price) }}</div>
+      </div>
+      <div class="flex justify-between" v-for="addon in Object.values(selectedAddons)" :key="addon.id">
+        <div class="text-sm text-neutral-muted">{{ addon.name }}
+          <span v-if="addon.quantity">X {{ addon.quantity }}</span>
+        </div>
+        <div class="text-sm text-neutral-muted">{{ usdFormatter.format(addon.price * (addon.quantity || 1)) }}</div>
+      </div>
+      <div v-if="isVIP && workshopDiscount" class="flex justify-between">
+        <div class="text-xs text-brand-emphasis">Workshop discount (VIP 10%)</div>
+        <div class="text-xs text-brand-emphasis">-{{ usdFormatter.format(workshopDiscount) }}</div>
+      </div>
+    </div>
+      <div class="flex justify-between">
+        <div class="text-sm text-neutral">Total</div>
+        <div class="text-sm text-neutral">{{ usdFormatter.format(totalPrice) }}</div>
+      </div>
   </div>
 </template>
