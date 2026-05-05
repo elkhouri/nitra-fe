39177c7e8c90be69af902b96a7322c102ade969f
Alex Chen <elkhouri1@gmail.com>
2026-05-05 19:13:35 +0800

    feat: refactor OrderSummary to use summary composable for discount and total price calculations

--- src/components/OrderSummary.vue
+++ src/components/OrderSummary.vue
@@ -2,38 +2,11 @@
 import { computed } from 'vue';
 import { useAttendee } from '../composables/attendee';
 import { useAddons } from '../composables/addons';
+import { useSummary } from '../composables/summary';
 
 const { ticketType } = useAttendee()
 const { selectedAddons } = useAddons();
-
-const isVIP = computed(() => ticketType.value && ticketType.value.id === 'vip');
-
-const workshopDiscount = computed(() => {
-  if (!isVIP.value) return 0
-
-  return Object.values(selectedAddons.value).reduce((discount, addon) => {
-    if (addon.category === 'workshop') {
-      return discount + addon.price * 0.1;
-    }
-    return discount;
-  }, 0);
-});
-
-const totalPrice = computed(() => {
-  if (!ticketType.value) return 0;
-
-  const addonsTotal = Object.values(selectedAddons.value).reduce((total, addon) => {
-    if (addon.quantity) {
-      return total + addon.price * addon.quantity;
-    } else {
-      return total + addon.price;
-    }
-  }, 0);
-  const discount = workshopDiscount.value;
-  const total = ticketType.value.price + addonsTotal - discount;
-
-  return total;
-});
+const { workshopDiscount, totalPrice } = useSummary()
 
 const usdFormatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
@@ -55,7 +28,7 @@ const usdFormatter = new Intl.NumberFormat('en-US', {
         </div>
         <div class="text-sm text-neutral-muted">{{ usdFormatter.format(addon.price * (addon.quantity || 1)) }}</div>
       </div>
-      <div v-if="isVIP && workshopDiscount" class="flex justify-between">
+      <div v-if="workshopDiscount" class="flex justify-between">
         <div class="text-xs text-brand-emphasis">Workshop discount (VIP 10%)</div>
         <div class="text-xs text-brand-emphasis">-{{ usdFormatter.format(workshopDiscount) }}</div>
       </div>
